ea4cb856379ccccd925d1257c061babfa223456e
bnitinraj <bnitinraj7@gmail.com>
2024-03-30 12:15:45 +0530

    fixed null values in ReviewMessageProducer

--- src/main/java/org/nr/reviewms/review/messaging/ReviewMessageProducer.java
+++ src/main/java/org/nr/reviewms/review/messaging/ReviewMessageProducer.java
@@ -4,8 +4,9 @@ import org.nr.reviewms.review.Review;
 import org.nr.reviewms.review.dto.ReviewMessage;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class ReviewMessageProducer {
     private final RabbitTemplate rabbitTemplate;
 
@@ -15,11 +16,11 @@ public class ReviewMessageProducer {
 
     public void sendMessage(Review review) {
         ReviewMessage reviewMessage = new ReviewMessage();
-        reviewMessage.setId(reviewMessage.getId());
-        reviewMessage.setTitle(reviewMessage.getTitle());
-        reviewMessage.setDescription(reviewMessage.getDescription());
-        reviewMessage.setRating(reviewMessage.getRating());
-        reviewMessage.setCompanyId(reviewMessage.getCompanyId());
+        reviewMessage.setId(review.getId());
+        reviewMessage.setTitle(review.getTitle());
+        reviewMessage.setDescription(review.getDescription());
+        reviewMessage.setRating(review.getRating());
+        reviewMessage.setCompanyId(review.getCompanyId());
         rabbitTemplate.convertAndSend("companyRatingQueue", reviewMessage);
     }
 }
